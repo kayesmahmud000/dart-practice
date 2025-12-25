009ca1547816223e416e8a064f6bf7349ca66712
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-12-25 18:13:43 +0600

    added the mixin extensions etc

---
+++ mixin.dart
@@ -0,0 +1,29 @@
+import 'lib/string_apis.dart';
+
+abstract interface class Tuner{
+  void tuneInstrument();
+}
+
+mixin Guitarist implements Tuner{
+  void playSong(){
+    tuneInstrument();
+    print('Strums guitar majestically');
+
+  }
+}
+
+class PunKRocker with Guitarist{
+  @override
+void tuneInstrument(){
+  print("Don't  bother, being out of tune is punk rock.");
+}
+}
+
+void main(){
+  PunKRocker().tuneInstrument();
+
+  var str ="456";
+  var sum = str.parseInt();
+  print(sum);
+}
+
