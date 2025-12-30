2095525558f77ab23fa13e3004a864d3dcb98a49
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-12-30 12:33:36 +0600

    added the async example

--- async_example.dart
+++ async_example.dart
@@ -18,23 +18,168 @@
 //   print("User Data is fetching..");
 //   // print(createOrderMessage());
 // }
-Future<void> printOrderMessage () async{
-  print('Awaiting user order..');
-  var order =await fetchUserOrder();
-  print('Your order is : $order');
+
+// Future<void> printOrderMessage () async{
+//   print('Awaiting user order..');
+//   var order =await fetchUserOrder();
+//   print('Your order is : $order');
+// }
+
+// Future<String> fetchUserOrder() {
+//   return Future.delayed(const Duration(seconds: 4), ()=>'Large Latte');
+// }
+
+// void main()async {
+//   countSeconds(4);
+//   await printOrderMessage();
+// }
+
+// void countSeconds(int s){
+//   for (int i=1; i<=s; i++){
+//     Future.delayed(Duration(seconds: i), ()=> print(i));
+//   }
+// }
+
+
+// Part 1
+// Call the provided async function fetchRole()
+// to return the user role.
+Future<String> reportUserRole()async{
+  // TODO: Implement the reportUserRole function here.
+  final role = await fetchRole();
+  return 'User role: $role' ;
+}
+// Part 2
+// TODO: Implement the reportLogins function here.
+// Call the provided async function fetchLoginAmount()
+// to return the number of times that the user has logged in.
+Future<String>reportLogins() async{
+  final logins = await fetchLoginAmount();
+  return 'Total number of logins: $logins';
 }
 
-Future<String> fetchUserOrder() {
-  return Future.delayed(const Duration(seconds: 4), ()=>'Large Latte');
-}
+// The following functions those provided to you to simulate
+// asynchronous operations that could take a while.
 
-void main()async {
-  countSeconds(4);
-  await printOrderMessage();
-}
+Future<String> fetchRole() => Future.delayed(_halfSecond, () => _role);
+Future<int> fetchLoginAmount() => Future.delayed(_halfSecond, () => _logins);
 
-void countSeconds(int s){
-  for (int i=1; i<=s; i++){
-    Future.delayed(Duration(seconds: i), ()=> print(i));
+// The following code is used to test and provide feedback on your solution.
+// There is no need to read or modify it.
+
+void main() async {
+  print('Testing...');
+  List<String> messages = [];
+  const passed = 'PASSED';
+  const testFailedMessage = 'Test failed for the function:';
+  const typoMessage = 'Test failed! Check for typos in your return value';
+  try {
+    messages
+      ..add(_makeReadable(
+          testLabel: 'Part 1',
+          testResult: await _asyncEquals(
+            expected: 'User role: administrator',
+            actual: await reportUserRole(),
+            typoKeyword: _role,
+          ),
+          readableErrors: {
+            typoMessage: typoMessage,
+            'null':
+                'Test failed! Did you forget to implement or return from reportUserRole?',
+            'User role: Instance of \'Future<String>\'':
+                '$testFailedMessage reportUserRole. Did you use the await keyword?',
+            'User role: Instance of \'_Future<String>\'':
+                '$testFailedMessage reportUserRole. Did you use the await keyword?',
+            'User role:':
+                '$testFailedMessage reportUserRole. Did you return a user role?',
+            'User role: ':
+                '$testFailedMessage reportUserRole. Did you return a user role?',
+            'User role: tester':
+                '$testFailedMessage reportUserRole. Did you invoke fetchRole to fetch the user\'s role?',
+          }))
+      ..add(_makeReadable(
+          testLabel: 'Part 2',
+          testResult: await _asyncEquals(
+            expected: 'Total number of logins: 42',
+            actual: await reportLogins(),
+            typoKeyword: _logins.toString(),
+          ),
+          readableErrors: {
+            typoMessage: typoMessage,
+            'null':
+                'Test failed! Did you forget to implement or return from reportLogins?',
+            'Total number of logins: Instance of \'Future<int>\'':
+                '$testFailedMessage reportLogins. Did you use the await keyword?',
+            'Total number of logins: Instance of \'_Future<int>\'':
+                '$testFailedMessage reportLogins. Did you use the await keyword?',
+            'Total number of logins: ':
+                '$testFailedMessage reportLogins. Did you return the number of logins?',
+            'Total number of logins:':
+                '$testFailedMessage reportLogins. Did you return the number of logins?',
+            'Total number of logins: 57':
+                '$testFailedMessage reportLogins. Did you invoke fetchLoginAmount to fetch the number of user logins?',
+          }))
+      ..removeWhere((m) => m.contains(passed))
+      ..toList();
+
+    if (messages.isEmpty) {
+      print('Success. All tests passed!');
+    } else {
+      messages.forEach(print);
+    }
+  } on UnimplementedError {
+    print(
+        'Test failed! Did you forget to implement or return from reportUserRole?');
+  } catch (e) {
+    print('Tried to run solution, but received an exception: $e');
   }
 }
+
+const _role = 'administrator';
+const _logins = 42;
+const _halfSecond = Duration(milliseconds: 500);
+
+// Test helpers.
+String _makeReadable({
+  required String testResult,
+  required Map<String, String> readableErrors,
+  required String testLabel,
+}) {
+  if (readableErrors.containsKey(testResult)) {
+    var readable = readableErrors[testResult];
+    return '$testLabel $readable';
+  } else {
+    return '$testLabel $testResult';
+  }
+}
+
+// Assertions used in tests.
+Future<String> _asyncEquals({
+  required String expected,
+  required dynamic actual,
+  required String typoKeyword,
+}) async {
+  var strActual = actual is String ? actual : actual.toString();
+  try {
+    if (expected == actual) {
+      return 'PASSED';
+    } else if (strActual.contains(typoKeyword)) {
+      return 'Test failed! Check for typos in your return value';
+    } else {
+      return strActual;
+    }
+  } catch (e) {
+    return e.toString();
+  }
+}
+
+// import 'dart:io';
+
+// Stream<int> stream = Stream.periodic(
+//   const Duration(seconds: 1),(i)=> 210 ).take(5);
+
+// void main(){
+//     stream.listen((value) {
+//     print(value);
+//   });
+// }
