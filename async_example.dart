50f9e777d3a4c187d5f3c12aa957c271abe2d002
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-12-30 11:13:26 +0600

    added the async example

--- async_example.dart
+++ async_example.dart
@@ -1,10 +1,20 @@
-String createOrderMessage(){
-  var order =fetchUserOrder();
-  return 'Your order is : $order';
+// Future<String> createOrderMessage() async{
+//   var order = await fetchUserOrder();
+//   return 'Your order is : $order';
+// }
+
+// Future <String>fetchUserOrder()=> Future.delayed(const Duration(seconds: 2), ()=> 'Large Latte');
+
+
+// Future<void> main() async{
+//   print('Fetching user order..');
+//   print(await createOrderMessage());
+// }
+Future <void> fetchUserOrder(){
+  return Future.delayed( const Duration(seconds: 4), ()=> throw Exception('Logout failed : user ID is invalid'));
 }
-
-Future <String>fetchUserOrder()=> Future.delayed(const Duration(seconds: 2), ()=> 'Large Latte');
-
 void main(){
-  print(createOrderMessage());
+  fetchUserOrder();
+  print("User Data is fetching..");
+  // print(createOrderMessage());
 }
