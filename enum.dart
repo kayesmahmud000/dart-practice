d15ca132bb967bb3fb070c0b0c4d0ee1e9a698ad
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2026-01-05 11:53:21 +0600

    added the enum

--- enum.dart
+++ enum.dart
@@ -1,25 +1,41 @@
-enum Gender{
- Male,
- Female,
- Other
+// enum Gender{
+//  Male,
+//  Female,
+//  Other
+// }
+
+// class Person {
+//   String? firstName;
+//   String? lastName;
+//   Gender? gender;
+
+//   Person(this.firstName, this.lastName, this.gender);
+
+
+//   void display(){
+//     print('First Name is $firstName');
+//     print('Last Name is $lastName');
+//     print('Gender is ${gender?.name}');
+
+//   }
+// }
+// void main(){
+//  Person person =Person("Aby", "Khan", Gender.Male);
+//  person.display();
+// }
+
+
+enum CompanyType {
+  soleProprietorship("Sole Proprietorship"),
+  partnership("Partnership"),
+  corporation("Corporation"),
+  limitedLiabilityCompany(" Limited Liability Company");
+  final String text;
+  const CompanyType(this.text);
+
 }
 
-class Person {
-  String? firstName;
-  String? lastName;
-  Gender? gender;
-
-  Person(this.firstName, this.lastName, this.gender);
-
-
-  void display(){
-    print('First Name is $firstName');
-    print('Last Name is $lastName');
-    print('Gender is ${gender?.name}');
-
-  }
-}
-void main(){
- Person person =Person("Aby", "Khan", Gender.Male);
- person.display();
+void main (){
+  CompanyType soleProprietorship =CompanyType.soleProprietorship;
+  print(soleProprietorship.text);
 }
