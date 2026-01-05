14d6912030dba9ab31154677a9c9f378ee77c695
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2026-01-05 12:41:37 +0600

    added the abstract

--- abstract.dart
+++ abstract.dart
@@ -1,33 +1,57 @@
-abstract class Shape {
-  int dim1 ,dim2;
+// abstract class Shape {
+//   int dim1 ,dim2;
  
-  Shape(this.dim1, this.dim2);
+//   Shape(this.dim1, this.dim2);
 
-  void area();
-}
+//   void area();
+// }
 
-class Rectangle extends Shape{
+// class Rectangle extends Shape{
 
-  Rectangle(int dim1 , int dim2): super(dim1, dim2);
+//   Rectangle(int dim1 , int dim2): super(dim1, dim2);
 
-  @override
-  void area(){
-    print('The Rectangle area is ${dim1*dim2}');
+//   @override
+//   void area(){
+//     print('The Rectangle area is ${dim1*dim2}');
 
-  }}
+//   }}
 
-  class Triangle extends Shape{
-    Triangle(int dim1, int dim2): super(dim1,dim2);
+//   class Triangle extends Shape{
+//     Triangle(int dim1, int dim2): super(dim1,dim2);
 
-    void area(){
-      print('The Triangle area is ${0.5* dim1*dim2}');
-    }
-  }
+//     void area(){
+//       print('The Triangle area is ${0.5* dim1*dim2}');
+//     }
+//   }
 
 
   void main(){
-    Rectangle rectangle =Rectangle(40, 20);
-    Triangle triangle = Triangle(20, 10);
-    triangle.area();
-    rectangle.area();
+    
+    BBL bbl= BBL("BBL ", 5.4);
+    bbl.interest();
+    bbl.display();
   }
+
+
+abstract class Bank{
+  String name;
+  double rate;
+  Bank(this.name, this.rate);
+
+  void interest();
+
+  void display(){
+print('Bank Name :$name');
+  }
+}
+
+class BBL extends Bank{
+  BBL(String name , double rate):super(name, rate);
+
+@override
+  void interest(){
+    print('The interest is : $rate');
+  }
+
+}
+
